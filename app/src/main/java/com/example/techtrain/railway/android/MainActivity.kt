3721b8e6fd77e9dee5881e5beb60ccd9c2a2de3c
kuma033 <nhayato28.gmail@email.com>
2022-05-05 01:22:36 +0900

    modified:   app/build.gradle 	modified:   app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt 	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+
+import com.example.techtrain.railway.android.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        findViewById<TextView>(R.id.text).text = "Hello My World!"
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.text.text = getString(R.string.text)
+//        findViewById<TextView>(R.id.text).text = "Hello!"
     }
 }
